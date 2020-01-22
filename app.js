88f501e8ae5bde823ee7f2c199d41e9fedb1137b
itope84 <topephysics@gmail.com>
2020-01-22 08:53:35 +0100

    initialised git and npm

---
+++ app.js
@@ -0,0 +1,14 @@
+const statements = [
+  "make it big",
+  "make it easy",
+  "innovate",
+  "make it faster",
+  "make it smarterout work the best",
+  "do the best",
+  "make it great",
+  "make it convenient",
+  "make it awesome",
+  "make it happen",
+  "make it count",
+  "make it brilliant"
+];
